c6cf5cbb79a9f5e2b9f05cdd610ce34a85a35113
George <auraa17@yandex.ru>
2026-03-03 21:08:54 +0100

    Bad script

---
+++ BadScript.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var Node2d
+	add_child(Node2d)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
