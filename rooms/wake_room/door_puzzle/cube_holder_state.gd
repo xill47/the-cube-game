c01babd5f502a9be92165ccae53b005953c0ecbe
George <auraa17@yandex.ru>
2026-03-15 16:59:51 +0100

    Setup

--- rooms/wake_room/door_puzzle/cube_holder_state.gd
+++ rooms/wake_room/door_puzzle/cube_holder_state.gd
@@ -12,10 +12,15 @@ static func create(_safe: CubeHolder) -> CubeHolderState:
 	return state
 
 func unlock_stand():
+	#When puzzle is solved
 	locked = false
 	on_changed()
 	holder_opened.emit()
 
 func interact() -> void:
-	if not locked:
+	if locked:
+		#Look at first side of the Cube
+		pass
+	else:
+		#Add Cube to the players inventory - Allow player to use Cube
 		cube_taken.emit()
