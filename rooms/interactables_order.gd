b7b1627edd17b546ab1216b8478ed97712958c3e
George <auraa17@yandex.ru>
2026-03-15 18:15:33 +0100

    Start corridor

--- rooms/interactables_order.gd
+++ rooms/interactables_order.gd
@@ -3,11 +3,16 @@ extends Node2D
 @export var character : Node2D
 
 var interactables : Array[Node]
+var room_empty := false
 
 func _ready() -> void:
 	interactables = get_children()
+	if interactables.is_empty():
+		room_empty = true
 
 func _process(_delta: float) -> void:
+	if room_empty:
+		return
 	for inter: Node2D in interactables:
 		if character.position.y < inter.position.y:
 			inter.z_index = 1
