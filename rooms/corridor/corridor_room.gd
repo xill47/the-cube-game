3d2698eb8ba2be97e76241bfbccb599e2cd1e25c
George <auraa17@yandex.ru>
2026-03-15 19:36:13 +0100

    Corridor done

--- rooms/corridor/corridor_room.gd
+++ rooms/corridor/corridor_room.gd
@@ -3,10 +3,5 @@ extends Room
 
 var corridor_state: CorridorState
 
-@onready var right_code: String = "1245"
-
 func get_state() -> CorridorState:
 	return corridor_state
-
-#All buttons pressed in the right order - open the CubeStand
-#If button unpressed - reset puzzle
