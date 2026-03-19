0fcbb4917e9f5f8f4c21a89ac6c59a2614327481
George <auraa17@yandex.ru>
2026-03-19 19:15:56 +0100

    Suggestions

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -5,6 +5,10 @@ extends Control
 @onready var sound_bus := AudioServer.get_bus_index("Sound")
 @onready var music_bus := AudioServer.get_bus_index("Music")
 
+func _ready() -> void:
+	if OS.has_feature("web"):
+		%ExitLabel.hide()
+
 func _on_option_label_pressed() -> void:
 	anim_player.play("open-options")
 
@@ -33,14 +37,13 @@ func _on_yes_button_pressed() -> void:
 func _on_check_box_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	if not toggled_on:
+	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "open-options":
-		anim_player.play("idle-options")
-	if anim_name == "open-exit":
-		anim_player.play("idle-exit")
-	if anim_name == "close-options" or anim_name == "close-exit":
-		anim_player.play("start")
+	match anim_name:
+		"open-options": anim_player.play("idle-options")
+		"open-exit": anim_player.play("idle-exit")
+		"close-options": anim_player.play("start")
+		"close-exit": anim_player.play("start")
