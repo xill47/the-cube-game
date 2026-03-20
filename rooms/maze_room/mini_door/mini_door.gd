5042deb54ec5eb0dacee3b34a01fe89d4064e949
George <auraa17@yandex.ru>
2026-03-21 00:52:17 +0100

    maze-room-setup1

---
+++ rooms/maze_room/mini_door/mini_door.gd
@@ -0,0 +1,22 @@
+class_name Door
+extends Interactable
+
+@export var starts_locked: bool = false
+@export_file_path("*.tscn") var starting_leads_to: String
+@export var leads_to_spawn: String
+
+var state: DoorState
+
+@onready var sprite := $Icon
+
+func _ready() -> void:
+	state.changed.connect(_on_state_changed)
+
+func get_state() -> InteractableState:
+	return state
+
+func _on_state_changed():
+	if state.locked:
+		sprite.frame_coords.x = 1
+	else:
+		sprite.frame_coords.x = 2
